25e975b06164b01b00e09ebbdb924754ce2173e8
Mauricio Lucas <mauricio.nq@hotmail.com>
2017-11-18 11:56:11 -0200

    welcome sem estilo

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import styles from './styles'
 
 export default class Welcome extends Component {
+  static navigationOptions = {
+    header: null,
+  };
+
   render(){
     return(
-      <View />
+      <View style={styles.container}>
+          <Text style={styles.welcomeTitle}></Text>
+          <Text style={styles.welcomeDescription}>
+            Para continuar, precisamos que voçê informe seu usuário no GitHub
+          </Text>
+
+          <TextInput
+            style={styles.input}
+            placeholder="Degite seu usuário"
+          />
+
+          <TouchableOpacity style={styles.button} onPress={() => {}}>
+            <Text style={styles.buttonText}>Prosseguir</Text>
+          </TouchableOpacity>
+      </View>
     );
   }
 };
