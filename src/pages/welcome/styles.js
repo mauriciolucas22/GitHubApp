bddeb106a3e9a7cbfe4e637863e0b4dd70794ed3
Mauricio Lucas <mauricio.nq@hotmail.com>
2017-11-18 12:07:34 -0200

    colors e fonts funcionando em welcome

--- src/pages/welcome/styles.js
+++ src/pages/welcome/styles.js
@@ -1,10 +1,11 @@
 import { StyleSheet } from 'react-native';
+import { colors, fonts } from '../../styles';
 
 const styles = StyleSheet.create({
   container: {
 
   },
-  
+
 });
 
 export default styles;
