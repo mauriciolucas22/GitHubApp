86298b0cd1f1e674b5a3013a668ae7619cc04c83
Mauricio Lucas <mauricio.nq@hotmail.com>
2017-11-20 09:56:35 -0200

    userExists configurado

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { AsyncStorage } from 'react-native';
+
 import Routes from './routes'
 import "./config/ReactotronConfig";
 
-const App = () => (
-  <Routes />
-);
 
-export default App;
+export default class App extends Component {
+
+  state = {
+    userExists: false,
+    userChecked: false,
+  }
+
+  componentWillMount(){
+    this.checkUser().then((response) => {
+      this.setState({ userExists: response, userChecked: true });
+    });
+  };
+
+  checkUser = async () => {
+    const user = await AsyncStorage.getItem('@GitHubApp:username');
+
+    return user !== null;
+  };
+
+  render() {
+    return (
+      <Routes />
+    );
+  }
+}
