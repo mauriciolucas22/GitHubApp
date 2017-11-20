81075c276559c5e7a54365909382c9de822b7eb6
Mauricio Lucas <mauricio.nq@hotmail.com>
2017-11-20 11:36:34 -0200

    logout user funcionando

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { NavigationActions } from 'react-navigation';
 import { colors } from "../../styles";
 
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, AsyncStorage } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import styles from './styles';
 
 export default class Header extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      dispath: PropTypes.func,
+    }).isRequired,
+  };
+
+  logout = () => {
+    AsyncStorage.removeItem('@GitHubApp:username').then(() => {
+      const { dispatch } = this.props.navigation;
+      const resetAction = NavigationActions.reset({
+        index: 0,
+        actions: [
+          NavigationActions.navigate({ routeName: 'Welcome' }),
+        ],
+      });
+      dispatch(resetAction);
+    });
+  };
+
+
   render(){
     return (
       <View style={styles.container}>
         <Text style={styles.title}>GitHubApp</Text>
 
-        <TouchableOpacity onPress={() => {}}>
+        <TouchableOpacity onPress={this.logout}>
             <Icon name="exchange" size={14} color={colors.primary} />
         </TouchableOpacity>
       </View>
